d8b0e22f1cb73a18107e782c4bc099be58ae73d5
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-06-01 20:11:22 -0300

    adiciona enunciados dos exercicios como comentario

--- ex01.js
+++ ex01.js
@@ -1,6 +1,19 @@
 const prompt = require("prompt-sync")();
 console.clear();
 
+/**  
+Reajuste salarial - As empresas @.com resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes. Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual: 
+Salários até R$ 280,00 (incluindo): aumento de 20%. 
+Salários entre R$ 280,00 e R$ 700,00: aumento de 15%.
+Salários entre R$ 700,00 e R$ 1500,00: aumento de 10%.
+Salários de R$ 1500,00 em diante: aumento de 5%.
+Após o aumento ser realizado, informe na tela:
+O salário antes do reajuste.
+O percentual de aumento aplicado.
+O valor do aumento.
+O novo salário, após o aumento.
+ */
+
 const reajusteAte280 = 20 / 100;
 const reajusteEntre280e700 = 15 / 100;
 const reajusteEntre700e1500 = 10 / 100;
--- ex02.js
+++ ex02.js
@@ -1,5 +1,10 @@
 const prompt = require("prompt-sync")();
 console.clear();
+/*
+ Jogo da adivinhação - Escreva um programa que faça o computador “pensar” em um número inteiro entre 0 e 10
+  e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador. 
+ O programa deverá escrever na tela se o usuário venceu ou perdeu.
+*/
 
 const max = 10;
 const min = 0;
